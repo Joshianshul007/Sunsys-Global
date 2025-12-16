67fd5a5f67ec5e8d3d1ed299b642ccf09ecb0807
Anshuman Joshi <joshianshul007@gmail.com>
2025-12-16 13:14:08 +0530

    commit changes

--- components/layout/HomepageWrapper.tsx
+++ components/layout/HomepageWrapper.tsx
@@ -4,40 +4,20 @@ import { ReactNode } from "react";
 
 export default function HomepageWrapper({ children }: { children: ReactNode }) {
   return (
-    <main
-      className="
-        relative
-        w-full
-        min-h-screen
-        text-white
-        overflow-x-hidden
+    <div
+  className="
+    relative w-full text-white
+    bg-[url('/background.jpg')]
+    bg-cover bg-center bg-no-repeat
+  "
+>
 
-        /* Background image */
-        bg-[url('/background.jpg')]
-        bg-no-repeat
-        bg-top
-        bg-[length:100%_auto]
-        md:bg-cover
-      "
-    >
-      {/* Subtle glow effect */}
-      <div
-        className="
-          pointer-events-none
-          absolute inset-0
-          bg-[radial-gradient(circle_at_top,_rgba(79,139,255,0.12),transparent_60%)]
-        "
-      />
+      {/* Optional subtle background effects */}
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_top,_rgba(79,139,255,0.12),transparent_60%)] pointer-events-none" />
 
-      {/* CONTENT WRAPPER */}
-      <div
-        className="
-          relative z-10
-          w-full
-        "
-      >
+      <div className="relative z-10">
         {children}
       </div>
-    </main>
+    </div>
   );
 }
